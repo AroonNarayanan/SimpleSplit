dc89460053805736ed244fabcac71e0305c7d2c1
Aroon Narayanan <aroon@gatech.edu>
2021-01-20 23:36:57 -0500

    Initial Commit

---
+++ SimpleSplit/SimpleSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleSplitApp.swift
+//  SimpleSplit
+//
+//  Created by Aroon Narayanan on 1/20/21.
+//
+
+import SwiftUI
+
+@main
+struct SimpleSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
